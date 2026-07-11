3df68191167ff956fd95758a0976d32394e4b753
DEV01ASM <erazo@agristar.com.mx>
2026-07-11 09:38:52 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from './platzi.webp'; /* habilitar el logo */
+import './App.css'; /* Habilitamos los estilos */
 
+/* Componente REACT Nota: Los componentes con jsx (js + xml)react inician con Mayusculas*/
 function App() {
   return (
     <div className="App">
+
+      <TodoItem/>{/* El componente react To-doItem insertado como un elemento */}
+      <TodoItem/>
+      <TodoItem/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +21,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
 }
+
+/* Componente React to-doItem */
+function TodoItem() {
+  return(
+    <li>
+      <span>V</span>
+        <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+  
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; /* Habilitamos lo que vamos a usar */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root')); /* Variable que llama a createRoot y pasa como parametro el id root */
+root.render(<App />); /* La instruccion de renderizar la etiqueta App */
