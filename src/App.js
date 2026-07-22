fb537669c73a7744de40e32c1106df2a15607886
DEV01ASM <erazo@agristar.com.mx>
2026-07-22 12:27:44 -0600

    feat:Muestra tareas segun la busqueda, subraya completados y elimina tareas

--- src/App.js
+++ src/App.js
@@ -18,13 +18,32 @@ function App() {
   /* === Estados === */
   const [todos, setTodos] = React.useState(defaultTodos);
   /* creamos un estado para lo que escriban los usuarios con valor inicial vacio*/
-  const [searchValue, setSearchValue] = React.useState(''); /* Manejamos el estado con useReactState */
+  const [searchValue, setSearchValue] = React.useState('');             /* Manejamos el estado con useReactState */
 
   /* === Estados Derivados === */
   const completedTodos = todos.filter(todo => !!todo.completed).length; /* Filtramos los todos completados y nos devuelve true o false (!!)*/
-  const totalTodos = todos.length; /* Contamos el total de todos */
+  const totalTodos = todos.length;                                      /* Contamos el total de todos */
   
-  console.log('Los usuarios buscan todos de: ' + searchValue); /* Mostramos en consola lo que escriben los usuarios */
+  const searchedTodos = todos.filter(todo => {                          /* buscamos las coincidencias en el texto de los to-do´s */
+    const todoText = todo.text.toLowerCase();                           /* obtenemos en minusculas el texto */
+    const searchText = searchValue.toLowerCase();                       /* obtenemos en minusculas la busqueda */
+    return todoText.includes(searchText);                               /* devolvemos la coincidencia */
+  });
+
+  /* === Funciones === */
+  const completeTodo = (text) => {                                      /* Funcion para completar to-do´s */
+    const newTodos = [...todos];  /* copia de todos los to-do´s */
+    
+    const todoIndex = newTodos.findIndex(todo => todo.text === text); /* Obtenemos el indice del to-do a modificar */
+    newTodos[todoIndex].completed = true; /* Modificamos el estado del to-do a true (completado) */
+    setTodos(newTodos);
+  }
+  const deleteTodo = (text) => {                                      /* Funcion para eliminar to-do´s */
+    const newTodos = [...todos];  /* copia de todos los to-do´s */
+    const todoIndex = newTodos.findIndex(todo => todo.text === text); /* Obtenemos el indice del to-do a eliminar */
+    newTodos.splice(todoIndex, 1); /* Eliminamos el to-do */
+    setTodos(newTodos);
+  }
   return (
     <>
       
@@ -32,11 +51,18 @@ function App() {
       <TodoSearch 
         searchValue={searchValue}
         setSearchValue={setSearchValue}
-      />{/* Props  */}
+      />{/* Props para escuchar lo que busca el usuario    */}
 
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        {/* Renderiza desde las to-do´s buscados */}
+        {searchedTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}
+            onComplete = {() => completeTodo(todo.text)} /* envuelve en AF para ejecutarse solo al dar click y no antes */
+            onDelete = {() => deleteTodo(todo.text)} /* envuelve en AF para ejecutarse solo al dar click y no antes */
+          />
         ))}
       </TodoList>
 
