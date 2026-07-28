bab914fea69b3eb1d7170f5a8e133e96645a4fe7
DEV01ASM <erazo@agristar.com.mx>
2026-07-28 17:23:43 -0600

    Implementamos localStorage en tareas

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const defaultTodos = [
+/* const defaultTodos = [
   {text: 'Llevar a caminar a garritas', completed: true},
   {text: 'Tomar el curso react.js', completed: false}, 
   {text: 'Surtir el super', completed: false},
   {text: 'Pagar el agua', completed: false},
   {text: 'Lavar el coche', completed: false},
-];
+]; 
+
+localStorage.setItem('CHECKLIST_V1', JSON.stringify(defaultTodos)); //Local Storage solo almacena strings, por eso usamos JSON.stringify para convertir el array en string
+*/
+
+//localStorage.removeItem('CHECKLIST_V1'); /* Eliminamos el localStorage para que no se guarde la informacion de los to-do´s */
 
 /* Componente REACT Nota: Los componentes con jsx (js + xml)react inician con Mayusculas*/
 function App() {
   /* === Estados === */
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('CHECKLIST_V1');       /* obtenemos las tareas del localStorage */
+
+  let parsedTodos;                                                      //inicializar
+
+  if (!localStorageTodos){                                              //Si esta vacio creamos un array vacio y lo guardamos en el localStorage
+    localStorage.setItem('CHECKLIST_V1', JSON.stringify([]));           //local storage sea un array vacio stringifiado
+    parsedTodos = [];
+
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);                        //PARSEAMOS el string del localStorage que encuentre
+  }
+
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   /* creamos un estado para lo que escriban los usuarios con valor inicial vacio*/
   const [searchValue, setSearchValue] = React.useState('');             /* Manejamos el estado con useReactState */
 
@@ -29,20 +47,26 @@ function App() {
     const searchText = searchValue.toLowerCase();                       /* obtenemos en minusculas la busqueda */
     return todoText.includes(searchText);                               /* devolvemos la coincidencia */
   });
+  /* === FUNCION === */
 
-  /* === Funciones === */
-  const completeTodo = (text) => {                                      /* Funcion para completar to-do´s */
-    const newTodos = [...todos];  /* copia de todos los to-do´s */
-    
-    const todoIndex = newTodos.findIndex(todo => todo.text === text); /* Obtenemos el indice del to-do a modificar */
-    newTodos[todoIndex].completed = true; /* Modificamos el estado del to-do a true (completado) */
-    setTodos(newTodos);
+  /* Funcion que reciba nuevas tareas que actualice al estado y al localStorage */
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('CHECKLIST_V1', JSON.stringify(newTodos));     // Guarda en localstorage
+    setTodos(newTodos);                                                //Guarda en estados
   }
-  const deleteTodo = (text) => {                                      /* Funcion para eliminar to-do´s */
+  
+  const completeTodo = (text) => {                                      /* Funcion para completar to-do´s */
+    const newTodos = [...todos];                                        /* copia de todos los to-do´s */
+    
+    const todoIndex = newTodos.findIndex(todo => todo.text === text);   /* Obtenemos el indice del to-do a modificar */
+    newTodos[todoIndex].completed = true;                               /* Modificamos el estado del to-do a true (completado) */
+    saveTodos(newTodos);
+  }
+  const deleteTodo = (text) => {                                        /* Funcion para eliminar to-do´s */
     const newTodos = [...todos];  /* copia de todos los to-do´s */
-    const todoIndex = newTodos.findIndex(todo => todo.text === text); /* Obtenemos el indice del to-do a eliminar */
+    const todoIndex = newTodos.findIndex(todo => todo.text === text);   /* Obtenemos el indice del to-do a eliminar */
     newTodos.splice(todoIndex, 1); /* Eliminamos el to-do */
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
   return (
     <>
